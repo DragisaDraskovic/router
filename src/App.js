8ab98a7718e5e9846a0ac40daa6c77f7a3901185
DragisaDraskovic <dragisadraskovic2@gmail.com>
2023-02-22 16:09:49 +0100

    finish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+// import { React, ReactDOM } from 'react'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Notes from './components/Notes'
+import Users from './components/Users'
+import Home from './components/Home'
 
-function App() {
+const App = () => {
+
+  const padding = {
+    padding: 5
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Router>
+      <div>
+        <Link style={padding} to='/'>home</Link>
+        <Link style={padding} to='/notes'>notes</Link>
+        <Link style={padding} to='/users'>users</Link>
+      </div>
 
-export default App;
+      <Routes>
+        <Route path='/notes' element={<Notes />} />
+        <Route path='/users' element={<Users />} />
+        <Route path='/' element={<Home />} />
+      </Routes>
+      <div>
+        <i>Note app, Deparmtent of Computer Science 2023</i>
+      </div>
+    </Router>
+  )
+}
+export default App
